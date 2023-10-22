1f58ff5fd3a0208e7d040acd631b0253811204fe
Benson Jing <bensonjing@icloud.com>
2023-10-22 07:30:17 -0700

    feat: setup routes

--- server/index.js
+++ server/index.js
@@ -3,12 +3,17 @@ import * as dotenv from 'dotenv'
 import cors from 'cors'
 
 import connectDB from './mongodb/connect.js'
+import PostRoutes from './routes/postRoutes.js'
+import DalleRoutes from './routes/dalleRoutes.js'
 
 dotenv.config()
 
 const app = express()
 app.use(cors())
 app.use(express.json({ limit: '50mb' }))
+
+app.use('api/v1/post', PostRoutes)
+app.use('api/v1/dalle', DalleRoutes)
 
 app.get('/', async (req, res) => {
   res.send('Hello, World!')
