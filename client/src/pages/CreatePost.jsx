d493f8f3cf742b43d47116a3f1e31f30bf38d60f
Benson Jing <bensonjing@icloud.com>
2023-10-21 23:09:37 -0700

    feat: setup create post page

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -15,16 +15,21 @@ export default function CreatePost() {
   const [generatingImg, setGeneratingImg] = useState(false)
   const [loading, setLoading] = useState(false)
 
+  const generateImage = () => {
+
+  }
+
   const handleSubmit = () => {
 
   }
 
   const handleChange = (e) => {
-
+    setForm({ ...form, [e.target.name]: e.target.value })
   }
 
   const handleSupriseMe = () => {
-
+    const randomPrompt = getRandomPrompt(form.prompt)
+    setForm({ ...form, prompt: randomPrompt })
   }
 
   return (
@@ -42,8 +47,8 @@ export default function CreatePost() {
             labelName="Your name"
             type="text"
             name="name"
-            placeholder="A plush toy robot sitting against a yellow wall"
-            value={form.prompt}
+            placeholder="John Doe"
+            value={form.name}
             handleChange={handleChange}
           />
 
@@ -51,12 +56,56 @@ export default function CreatePost() {
             labelName="Prompt"
             type="text"
             name="prompt"
-            placeholder=""
-            value={form.name}
+            placeholder="A plush toy robot sitting against a yellow wall"
+            value={form.prompt}
             handleChange={handleChange}
             isSupriseMe
             handleSupriseMe={handleSupriseMe}
           />
+
+          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+            {form.phot ? (
+              <img
+                src={form.phot}
+                alt={form.prompt}
+                className="w-full h-full object-contain"
+              />
+            ) : (
+              <img
+                src={preview}
+                alt="preview"
+                className="w-9/12 h-9/12 object-contain opacity-40"
+              />
+            )}
+
+            {generatingImg && (
+              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+                <Loader />
+              </div>
+            )}
+          </div>
+        </div>
+
+        <div className="mt-5 flex gap-5">
+          <button
+            type="button"
+            onClick={generateImage}
+            className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {generatingImg ? 'Generating...' : 'Generate'}
+          </button>
+        </div>
+
+        <div className="mt-10">
+          <p className="mt-2 text-[#666e75] text-[14px]">
+            Once you have created the image, you can share it with the community
+          </p>
+          <button
+            type="button"
+            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {loading ? 'Sharing...' : 'Share with the community'}
+          </button>
         </div>
       </form>
     </section>
