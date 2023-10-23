93df227d910e3d0e0116010366fd62ab515288c8
Benson Jing <bensonjing@icloud.com>
2023-10-22 19:13:40 -0700

    feat: connect client to post route

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -20,6 +20,34 @@ export default function Home() {
 
   const [searchText, setSearchText] = useState("")
 
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true)
+
+      try {
+        const response = await fetch('http://192.168.0.208:8080/post', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+        })
+
+        if (response.ok) {
+          const result = await response.json()
+
+          setAllPosts(result.data.reverse())
+        }
+      } catch (err) {
+        alert(err)
+      } finally {
+        setLoading(false)
+      }
+
+    }
+
+    fetchPosts()
+  }, [])
+
   return (
     <section className="max-w-7xl mx-auto">
       <div>
@@ -54,7 +82,7 @@ export default function Home() {
                 />
               ) : (
                 <RenderCards
-                  data={[]}
+                  data={allPosts}
                   title="No posts found"
                 />
               )}
